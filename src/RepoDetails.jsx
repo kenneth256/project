92bd00fbc5ce8ff7523a993afcc6c7ff284a562f
kenneth david <kennethdavid256.com>
2024-04-22 11:31:34 +0300

    my project

--- src/RepoDetails.jsx
+++ src/RepoDetails.jsx
@@ -1,19 +1,20 @@
 // RepositoryDetailsPage.jsx
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function RepoDetails() {
   const { repoId } = useParams();
   const [repository, setRepository] = useState(null);
 
   useEffect(() => {
-    axios.get(`https://api.github.com/repositories/${repoId}`)
-      .then(response => {
+    axios
+      .get(`https://api.github.com/repositories/${repoId}`)
+      .then((response) => {
         setRepository(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching repository details:', error);
+      .catch((error) => {
+        console.error("Error fetching repository details:", error);
       });
   }, [repoId]);
 
@@ -22,7 +23,7 @@ function RepoDetails() {
   }
 
   return (
-    <div className='Details'>
+    <div className="Details">
       <h2>{repository.name}</h2>
       <p>Description: {repository.description}</p>
       <p>Stars: {repository.stargazers_count}</p>
@@ -30,20 +31,21 @@ function RepoDetails() {
       <p>Language: {repository.language}</p>
       <p>Created At: {repository.created_at}</p>
       <p>Last Updated At: {repository.updated_at}</p>
-      <p>License: {repository.license ? repository.license.name : 'No License'}</p>
+      <p>
+        License: {repository.license ? repository.license.name : "No License"}
+      </p>
       <p>Owner: {repository.owner.login}</p>
-      <p>Homepage: {repository.homepage || 'No Homepage'}</p>
+      <p>Homepage: {repository.homepage || "No Homepage"}</p>
       <p>Size: {repository.size} KB</p>
       <p>Watchers: {repository.watchers}</p>
       <p>Subscribers: {repository.subscribers_count}</p>
       <p>Network Count: {repository.network_count}</p>
       <p>Open Issues: {repository.open_issues_count}</p>
-      <p>Is Private: {repository.private ? 'Yes' : 'No'}</p>
-      <p>Archived: {repository.archived ? 'Yes' : 'No'}</p>
-      <a href="./index.html">
-  <button>Back to Homepage</button>
-</a>
-
+      <p>Is Private: {repository.private ? "Yes" : "No"}</p>
+      <p>Archived: {repository.archived ? "Yes" : "No"}</p>
+      <a href="https://charming-daifuku-b3a0de.netlify.app/">
+        <button>Back to Homepage</button>
+      </a>
     </div>
   );
 }
