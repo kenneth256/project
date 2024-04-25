75ba547c2913934bd0d84beb9c4653a6baa98a3c
kenneth david <kennethdavid256.com>
2024-04-25 15:38:50 +0300

    alt-project

--- src/RepositoriesList.jsx
+++ src/RepositoriesList.jsx
@@ -1,41 +1,4 @@
-// // RepositoriesPage.jsx
-// import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-// import axios from 'axios';
-// import { each } from 'jquery';
-// import Btns from './button';
-// import $ from 'jquery';
 
-
-// function RepositoriesList() {
-//   const [repositories, setRepositories] = useState([]);
-
-//   useEffect(() => {
-//     axios.get('https://api.github.com/users/kenneth256/repos')
-//       .then(response => {
-//         setRepositories(response.data);
-//       })
-//       .catch(error => {
-//         console.error('Error fetching repositories:', error);
-//       });
-//   }, []);
-
-//   return (
-//     <div style={{display: 'flex',marginLeft:'10px', flexDirection:'column', color:''}}>
-//       <h1>My GitHub Repositories</h1>
-//       <Btns />
-//       <ul>
-//         {repositories.map(each => (
-//           <li key={each.id} style={{marginLeft: '10px'}}>
-//             <Link to={`/repo/${each.id}`}>{each.name}</Link>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default RepositoriesList;
 
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
@@ -43,13 +6,16 @@ import axios from 'axios';
 import Btns from './button.jsx';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
-
 function RepositoriesList() {
   const [repositories, setRepositories] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [perPage] = useState(5); 
 
   useEffect(() => {
+    fetchRepositories();
+  }, [currentPage, perPage]);
+
+  const fetchRepositories = () => {
     axios.get(`https://api.github.com/users/kenneth256/repos?page=${currentPage}&per_page=${perPage}`)
       .then(response => {
         setRepositories(response.data);
@@ -57,7 +23,7 @@ function RepositoriesList() {
       .catch(error => {
         console.error('Error fetching repositories:', error);
       });
-  }, [currentPage, perPage]);
+  };
 
   const goToPreviousPage = () => {
     setCurrentPage(prevPage => prevPage - 1);
@@ -67,10 +33,14 @@ function RepositoriesList() {
     setCurrentPage(prevPage => prevPage + 1);
   };
 
+  const handleCreateRepo = (newRepo) => {
+    setRepositories(prevRepos => [newRepo, ...prevRepos]);
+  };
+
   return (
     <div style={{ display: 'flex', marginLeft: '10px', flexDirection: 'column', color: '' }}>
       <h1>My GitHub Repositories</h1>
-      <Btns />
+      <Btns onCreateRepo={handleCreateRepo} />
       <ul>
         {repositories.map(repo => (
           <li key={repo.id} style={{ marginLeft: '10px' }}>
